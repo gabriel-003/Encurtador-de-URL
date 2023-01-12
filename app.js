5e374fc965d2ec16765929aa23af7aeeb22e8613
Gabriel Brasil <gabrielfmbrasil03@gmail.com>
2023-01-12 19:23:29 -0300

    adicionando database com mongodb atlas e node_modules

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const urlCurta = require('./models/urlCurta')
+require("dotenv").config();
 const app = express();
 
-mongoose.connect('mongodb://localhost/urlShortener', {
+mongoose.connect(`mongodb+srv://dbUser:${process.env.DBUSER_PASSWORD}@cluster0.zustmvr.mongodb.net/?retryWrites=true&w=majority`, {
     useNewUrlParser: true, useUnifiedTopology: true
 });
 
@@ -30,4 +31,4 @@ app.get('/:urlCurta', async (req, res) => {
     res.redirect(urlEncurtada.completa);
 });
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
